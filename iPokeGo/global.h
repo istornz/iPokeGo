86c5937791e281762132203fdfdd86f2300096cb
Luis Alvarado <luis_alvarado95@outlook.com>
2016-07-25 17:12:47 -0600

    Added: High resolution Pokemon icons

--- iPokeGo/global.h
+++ iPokeGo/global.h
@@ -25,6 +25,10 @@
 #define TEAM_RED            2
 #define TEAM_YELLOW         3
 
+#define SPRITESHEET_COLS    7
+#define SPRITE_SIZE         65
+#define IMAGE_SIZE          32.5
+
 #define SERVER_API_DATA     @"http://%%server_addr%%/raw_data?pokemon=%%pokemon_display%%&pokestops=%%pokestops_display%%&gyms=%%gyms_display%%"
 #define SERVER_API_LOCA     @"http://%%server_addr%%/next_loc?lat=%%latitude%%&lon=%%longitude%%"
 
