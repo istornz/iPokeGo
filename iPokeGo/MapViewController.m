2915a6e207b4781a45b9273a210635f740ecad95
Curtis Herbert <me@curtisherbert.com>
2016-07-31 02:18:37 -0400

    More cleaning

--- iPokeGo/MapViewController.m
+++ iPokeGo/MapViewController.m
@@ -51,8 +51,6 @@
     [self loadNavBar];
     [self loadAnimatedImages];
     
-    self.mapview.zoomEnabled = true;
-    
     UILongPressGestureRecognizer *longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPressGesture:)];
     [self.mapview addGestureRecognizer:longPressGesture];
 }
@@ -614,30 +612,30 @@
 
 -(void)loadAnimatedImages
 {
-    self.animatedPokestopLured = [NSArray arrayWithObjects:[UIImage imageNamed:@"Pokespot-Lured_0023_Frame-1.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0022_Frame-2.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0021_Frame-3.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0020_Frame-4.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0019_Frame-5.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0018_Frame-6.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0017_Frame-7.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0016_Frame-8.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0015_Frame-9.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0014_Frame-10.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0013_Frame-11.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0012_Frame-12.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0011_Frame-13.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0010_Frame-14.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0009_Frame-15.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0008_Frame-16.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0007_Frame-17.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0006_Frame-18.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0005_Frame-19.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0004_Frame-20.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0003_Frame-21.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0002_Frame-22.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0001_Frame-23.png"],
-                                  [UIImage imageNamed:@"Pokespot-Lured_0000_Frame-24.png"], nil];
+    self.animatedPokestopLured = @[[UIImage imageNamed:@"Pokespot-Lured_0023_Frame-1.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0022_Frame-2.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0021_Frame-3.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0020_Frame-4.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0019_Frame-5.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0018_Frame-6.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0017_Frame-7.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0016_Frame-8.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0015_Frame-9.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0014_Frame-10.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0013_Frame-11.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0012_Frame-12.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0011_Frame-13.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0010_Frame-14.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0009_Frame-15.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0008_Frame-16.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0007_Frame-17.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0006_Frame-18.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0005_Frame-19.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0004_Frame-20.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0003_Frame-21.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0002_Frame-22.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0001_Frame-23.png"],
+                                   [UIImage imageNamed:@"Pokespot-Lured_0000_Frame-24.png"]];
 }
 
 #pragma mark - Actions
